8a7ec782127b3219599534815caabf0063438903
luyang <lliu240@student.monash.edu>
2021-12-05 17:35:43 +1100

    added gitpython package as a pip installation requirement
    Changes to be committed:
    	modified:   requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,7 @@
 pip install virtualenv
 python3 -m venv env
 source env/bin/activate
+pip install GitPython
 pip install chardet # 4.0.0
 pip install pylint
 pip install torch torchvision torchaudio
