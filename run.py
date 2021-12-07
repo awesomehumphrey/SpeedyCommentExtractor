52aa76402db237cbf4a7f1faa201e124525590f4
luyang <lliu240@student.monash.edu>
2021-12-07 20:12:19 +1100

    Added support for getting comment directly from repository
    Changes to be committed:
    	modified:   run.py

--- run.py
+++ run.py
@@ -2,25 +2,32 @@ import app
 import sys
 
 ###############################################################################
-#               The driver for the comment-extractor application              #
+#               The run for the comment-extractor application              #
 ###############################################################################
 
 length = len(sys.argv)
 
 if length == 0:
-  print('usage: driver.py <command> <filename or repository')
+  print('usage: run.py <command> <filename or repository')
   exit(0)
 
 if length == 2 and sys.argv[1] in ('-h', '--help'):
-  print('usage: \n driver.py <command> <filename or repository')
-  print('To get comments from directory: \n python3 driver.py -d <root directory>')
-  print('To get comments from repositories: \n python3 driver.py -repo <repository link> <branch name> <depth>')
+  print('usage: \n run.py <command> <filename or repository')
+  print('To get comments from directory: \n python3 run.py -d <root directory>')
+  print('To get comments from repositories: \n python3 run.py -repo <repository link> <branch name> <depth>')
 
-elif length == 3:
+elif length >= 3:
     command1 = sys.argv[1]
     directory = sys.argv[2]
     if command1 == '-d':
-        app.get_comment_from_path_using_all_languages(directory, './')
+      app.get_comment_from_path_using_all_languages(directory, './')
+    elif command1 == '-repo':
+      if length < 4:
+        Exception("Not enough arguments")
+      else:
+        repo = sys.argv[2]
+        branch = sys.argv[3]
+        app.get_comment_from_repo_using_all_languages(repo , branch, './')
 
 
 # app.get_comment_from_repo_using_all_languages("https://github.com/k9mail/k-9.git", "main", "./commentfiles/")
