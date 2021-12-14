fe12f0a1a507f612b881fcbee407637e3ddeb170
Luyang Liu <https://github.com/luyangliuable>
2021-12-14 15:44:55 +1100

    Added get synonym and get antonyms functionality

--- src/keyword-filter.py
+++ src/keyword-filter.py
@@ -1,8 +1,8 @@
 import os
 import csv
+import json
 import tempfile
 import chardet
-import json
 from typing import TypeVar, Generic, List, NewType
 
 T = TypeVar("T")
@@ -10,16 +10,24 @@ T = TypeVar("T")
 class keywordFilter():
     allLines = []
     dictLocation = "./keyword-dictionaries/keywords-maxed-out.JSON"
+    dictionary = ""
 
     def __init__(self, csvfile: str) -> None:
         self.file = csvfile
         self.openFile()
         self.getKeywords()
+        print(self.getSynonyms("choosing own goals"))
+        print(self.getAntonyms("choosing own goals"))
+
+    def getSynonyms(self, value: str) -> str:
+        return self.dictionary[value]["synonyms"]
+
+    def getAntonyms(self, value: str) -> str:
+        return self.dictionary[value]["antonyms"]
 
     def getKeywords(self) -> List[T]:
         with open(self.dictLocation, encoding="utf-8") as dictionaryFile:
-            dictionary = json.load(dictionaryFile)
-            print(dictionary)
+            self.dictionary = json.load(dictionaryFile)
 
 
     def openFile(self) -> None:
