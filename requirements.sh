b3edd092b4bc14d72e9d666f4fb6a9692387d408
Luyang Liu <https://github.com/luyangliuable>
2021-12-25 08:30:39 +1100

    Added pandas, mattplotlib, numpy, seaborn, sklearn and nltk as requirements for this projects
    Changes to be committed:
    	modified:   requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -8,4 +8,10 @@ pip install GitPython
 pip install chardet # 4.0.0
 pip install pylint
 pip install shutil
+pip install pandas
+pip install matplotlib
+pip install numpy
+pip install seaborn
+pip install sklearn
+pip install nltk
 pip install torch torchvision torchaudio
